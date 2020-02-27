68db20a743494fc9d00b3126cb20e8604b2ee0ed
wuchenyv1990 <350200039@qq.com>
2020-02-27 18:05:51 +0800

    unit test error

--- rabbit-delay-publisher/src/test/java/com/wcyv90/rabbit/delay/publisher/domain/XMessageSenderTest.java
+++ rabbit-delay-publisher/src/test/java/com/wcyv90/rabbit/delay/publisher/domain/XMessageSenderTest.java
@@ -3,16 +3,17 @@ package com.wcyv90.rabbit.delay.publisher.domain;
 import com.wcyv90.rabbit.message.XMessage;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import static com.wcyv90.rabbit.delay.publisher.infra.config.DelayConfig.DelayType.PLUGIN;
 import static org.mockito.Mockito.doNothing;
 
+@RunWith(MockitoJUnitRunner.class)
 public class XMessageSenderTest {
 
     @InjectMocks
@@ -21,17 +22,18 @@ public class XMessageSenderTest {
     @Mock
     private RabbitTemplate rabbitTemplate;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @Test
     public void shouldSendDelayMsgSuccess() {
         XMessage xMessage = new XMessage();
         String exchange = RandomStringUtils.random(8);
         String route = RandomStringUtils.random(8);
         long milliseconds = RandomUtils.nextLong(1, 10);
+
+        doNothing().when(rabbitTemplate).convertAndSend(exchange, route, message -> {
+            message.getMessageProperties().setHeader("x-delay", milliseconds);
+            return message;
+        });
+
         xMessageSender.sendDelayMsg(
                 xMessage,
                 exchange,
@@ -39,10 +41,6 @@ public class XMessageSenderTest {
                 PLUGIN,
                 milliseconds
         );
-        doNothing().when(rabbitTemplate).convertAndSend(exchange, route, message -> {
-            message.getMessageProperties().setHeader("x-delay", milliseconds);
-            return message;
-        });
     }
 
 }
